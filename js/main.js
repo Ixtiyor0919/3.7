3d6434c21d3cfb183ea8ec1261e4effd820d0dae
Ixtiyor0919 <Komiljonov2002@outlook.com>
2022-05-12 17:38:24 +0500

    function

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Currency exchange
+var elForm = document.querySelector('.form');
+var elNameInput = elForm.querySelector('.form__control');
+var elResultBox = elForm.querySelector('.form__resultbox');
+var elUsdSelect = elForm.querySelector('.form__select');
+var usd = (`11141.80`);
+var rubl = (`161.48`);
+var euro = (`11757.94`);
+
+elForm.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    elNameInput.value.trim();
+    if(elUsdSelect.value === "usd") {
+        elResultBox.textContent = `${(elNameInput.value / (usd)).toFixed(0)} $`;
+    }else if(elUsdSelect.value == "rubl") {
+        elResultBox.textContent = `${(elNameInput.value / (rubl)).toFixed(0)} P`;
+    }else if(elUsdSelect.value == "euro") {
+        elResultBox.textContent = `${(elNameInput.value / (euro)).toFixed(0)} €`;
+    };
+    // elResultBox.textContent =  elNameInput.value;
+    // elResultBox.textContent = elUsdSelect.value;
+    // var totalExpance = (elNameInput.value / elUsdSelect.value).toFixed(0);
+    // elResultBox.textContent = totalExpance;
+    elNameInput.value = '';
+    elUsdSelect.value = '';
+})
