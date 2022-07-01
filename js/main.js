6fa448e2b90cfbde59cc373663a7c6978b2722f2
Ixtiyor0919 <Komiljonov2002@outlook.com>
2022-07-02 00:29:53 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 //Currency exchange
 var elForm = document.querySelector('.form');
 var elNameInput = elForm.querySelector('.form__control');
-var elResultBox = elForm.querySelector('.form__resultbox');
+var elResultBox = elForm.querySelector('.result-valyuta');
 var elUsdSelect = elForm.querySelector('.form__select');
 var usd = document.querySelector('.valyuta-usd')
 var rubl = document.querySelector('.valyuta-rubl')
 var euro = document.querySelector('.valyuta-euro')
+var dataValyuta = document.querySelector('.data')
 
 getValyuta().then(result => {
     result.filter(item => {
+        if(item.date) {
+            dataValyuta.textContent = item.date
+        }
         if(item.code === "USD") {
             usd.textContent = item.cb_price
         }
@@ -30,6 +34,7 @@ elForm.addEventListener('submit', (evt) => {
         elNameInput.style.border = '2px solid white';
         elNameInput.style.background = 'red';
     }else {
+        elNameInput.style.background = '#215eac'
         elResultBox.classList.add('valyuta-tranlate')
     }
 
@@ -37,7 +42,7 @@ elForm.addEventListener('submit', (evt) => {
     getValyuta().then(result => {
         result.find(item => {
             if(selectValue == item.code) {
-                elResultBox.textContent = `${(currencyInput / (item.cb_price)).toFixed(2)} $`;
+                elResultBox.textContent = `${(currencyInput / (item.cb_price)).toFixed(2)}`;
             }
         })
     });
